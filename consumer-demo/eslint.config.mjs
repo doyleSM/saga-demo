56a44718939903635977c42717177fbb4b207121
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2025-04-13 16:35:13 -0300

    style: update style conf

--- consumer-demo/eslint.config.mjs
+++ consumer-demo/eslint.config.mjs
@@ -28,7 +28,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      "indent": ["error", "space", { "SwitchCase": 1 }],
+      "prettier/prettier": [2, { "useTabs": false }]
     },
   },
 );
